ca611cce98866944850f8755030ad74457608f52
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-11-26 19:34:04 +0700

    feat: create main package

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,54 @@
 package main;
 
-public class GamePanel {
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements MouseListener {
+
+	/**
+	 * 
+	 */
+	public static final int WIDTH = 1440;
+	public static final int HEIGHT = 800;
+	
+	public GamePanel() {
+		super();
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.addMouseListener(this);
+		this.setFocusable(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
   
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,14 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
-  }
+	public static void main(String[] args) {
+		JFrame window = new JFrame("Slice It!");
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setContentPane(new GamePanel());
+		window.pack();
+		window.setVisible(true);
+	}
 }
