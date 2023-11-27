606018a64431e12311f4fb8121483dc5a165d5c7
FaaizHaikal <faaizhaikal@gmail.com>
2023-11-27 14:56:32 +0700

    feat: create state manager

--- src/state/State.java
+++ src/state/State.java
@@ -1,5 +1,16 @@
 package state;
 
 public abstract class State {
-  
+  protected StateManager stateManager;
+
+  public abstract void init();
+  public abstract void update();
+  public abstract void draw(java.awt.Graphics2D g);
+  public abstract void mouseClicked(java.awt.event.MouseEvent e);
+  public abstract void mousePressed(java.awt.event.MouseEvent e);
+  public abstract void mouseReleased(java.awt.event.MouseEvent e);
+  public abstract void mouseEntered(java.awt.event.MouseEvent e);
+  public abstract void mouseExited(java.awt.event.MouseEvent e);
+  public abstract void mouseMoved(java.awt.event.MouseEvent e);
+  public abstract void mouseDragged(java.awt.event.MouseEvent e);
 }
--- src/state/StateManager.java
+++ src/state/StateManager.java
@@ -1,5 +1,117 @@
 package state;
 
 public class StateManager {
-  
+  private int currState;
+  private State[] states;
+
+  public static final int STATES = 3;
+  public static final int MENU_STATE = 0;
+  public static final int GAME_PLAY_STATE = 1;
+  public static final int GAME_OVER_STATE = 2;
+
+  public StateManager() {
+    currState = GAME_OVER_STATE;
+    states = new State[STATES];
+    loadState(currState);
+  }
+
+  public void loadState(int state) {
+    switch (state) {
+      case MENU_STATE:
+        states[MENU_STATE] = new MenuState(this);
+        break;
+      // case GAME_PLAY_STATE:
+      //   states[GAME_PLAY_STATE] = new GamePlayState(this);
+      //   break;
+      case GAME_OVER_STATE:
+        states[GAME_OVER_STATE] = new GameOverState(this);
+        break;
+      default:
+        System.out.println("Invalid state");
+        System.exit(1);
+        break;
+    }
+  }
+
+  public void unloadState(int state) {
+    states[state] = null;
+  }
+
+  public void setState(int state) {
+    unloadState(currState);
+    currState = state;
+    loadState(currState);
+  }
+
+  public void update() {
+    try {
+      states[currState].update();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void draw(java.awt.Graphics2D g) {
+    try {
+      if (states[currState] != null) states[currState].draw(g);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void mouseClicked(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mouseClicked(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void mousePressed(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mousePressed(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void mouseReleased(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mouseReleased(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void mouseEntered(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mouseEntered(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void mouseExited(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mouseExited(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void mouseMoved(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mouseMoved(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public void mouseDragged(java.awt.event.MouseEvent e) {
+    try {
+      states[currState].mouseDragged(e);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
 }
