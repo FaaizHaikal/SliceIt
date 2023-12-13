1e498303d5fe4d3767d8ca09b503a45d48a0acfc
FaaizHaikal <faaizhaikal@gmail.com>
2023-12-13 17:20:01 +0700

    feat: throw random elements

--- src/state/GamePlayState.java
+++ src/state/GamePlayState.java
@@ -1,7 +1,6 @@
 package state;
 
-import entitites.Element;
-import entitites.Apple;
+import entitites.*;
 import utilities.Background;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,40 +8,51 @@ import java.util.Random;
 
 public class GamePlayState extends State {
   private Background background;
-  private List<Element> elements = new ArrayList<Element>();
-  private Element element;
+  private static List<Element> elements;
   private static Random random = new Random();
-  private static final int BOMB = 0;
-  private static final int APPLE = 1;
-  private static final int KIWI = 2;
-  private static final int LEMON = 3;
-  private static final int ORANGE = 4;
-  private static final int POM = 5;
+  private static final int APPLE = 0;
+  private static final int KIWI = 1;
+  private static final int LEMON = 2;
+  private static final int ORANGE = 3;
+  private static final int POM = 4;
+  private static final int MAX_ELEMENT = 4;
 
   public GamePlayState(StateManager stateManager) {
     this.stateManager = stateManager;
 
     try {
       background = new Background("/background/background.jpg");
-      element = new Apple();
+      elements = new ArrayList<Element>();
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   public static void generateElement() {
-    int maxElement = random.nextInt(6) + 1;
+    int maxElement = random.nextInt(3) + 1;
 
     for (int i=0; i<maxElement; i++) {
+      if (elements.size() >= MAX_ELEMENT) break;
       int element = random.nextInt(6);
 
       switch (element) {
-        case BOMB:
         case APPLE:
+          elements.add(new Apple());
+          break;
         case KIWI:
+          elements.add(new Kiwi());
+          break;
         case LEMON:
+          elements.add(new Lemon());
+          break;
         case ORANGE:
+          elements.add(new Orange());
+          break;
         case POM:
+          elements.add(new Pom());
+          break;
+        default:
+          elements.add(new Bomb());
           break;
       }
     }
@@ -59,14 +69,20 @@ public class GamePlayState extends State {
     reDraw(g);
 
     background.draw(g);
-    element.draw(g);
+    generateElement();
+    for (int i=0; i<elements.size();i++) {
+      if (elements.get(i).isFall()) {
+        if (elements.get(i).isBomb()) System.err.println("BOMB REMOVED");
+        elements.remove(i);
+      } else {
+        elements.get(i).draw(g);
+      }
+    }
   }
 
   @Override
   public void mouseClicked(java.awt.event.MouseEvent e) {}
-  public void mousePressed(java.awt.event.MouseEvent e) {
-    // element.slice();
-  }
+  public void mousePressed(java.awt.event.MouseEvent e) {}
   public void mouseReleased(java.awt.event.MouseEvent e) {}
   public void mouseEntered(java.awt.event.MouseEvent e) {}
   public void mouseExited(java.awt.event.MouseEvent e) {}
@@ -75,11 +91,15 @@ public class GamePlayState extends State {
     int x = e.getX();
     int y = e.getY();
 
-    
-    // for (Element element : elements) {
-      if (element.getX() - 50 < x && x < element.getX() + 50 && element.getY() - 50 < y && y < element.getY() + 50) {
-        if (!element.getSliced()) element.slice();  
+    for (int i=0; i<elements.size(); i++){
+      Element element = elements.get(i);
+      if (element.getX() - 50 < x  && x < element.getX() + 50 && element.getY() - 50 < y && y < element.getY() + 50) {
+        if (element.isBomb()) {
+          stateManager.setState(StateManager.GAME_OVER_STATE);
+        } else if (!element.isSliced()) {
+          element.slice();
+        }
       }
-    // }
+    }
   }
 }
